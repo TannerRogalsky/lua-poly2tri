70ca23caaf6fcee4eb91b2bdf7f99dc3bd14f11f
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-03-03 15:29:44 -0500

    more triangulation

--- src/lua-poly2tri.cpp
+++ src/lua-poly2tri.cpp
@@ -8,7 +8,38 @@
 static int api_triangulate(lua_State *L)
 {
   int args = lua_gettop(L);
-  luaL_check(args >= 6, "Pass at least 3 vertices to triangulate.", args);
+  luaL_check(args >= 6, "need more vertices '%d'", args);
+
+  std::vector<p2t::Point*> verts;
+  for (int i = 1; i <= args; i += 2)
+    verts.push_back(new p2t::Point(lua_tonumber(L, i), lua_tonumber(L, i + 1)));
+
+  p2t::CDT* cdt = new p2t::CDT(verts);
+  cdt->Triangulate();
+  std::vector<p2t::Triangle*> tris = cdt->GetTriangles();
+  lua_newtable(L);
+  for (size_t i = 0; i < tris.size(); ++i)
+  {
+    lua_newtable(L);
+
+    lua_pushnumber(L, tris[i]->GetPoint(0)->x);
+    lua_rawseti(L, -2, 1);
+    lua_pushnumber(L, tris[i]->GetPoint(0)->y);
+    lua_rawseti(L, -2, 2);
+    lua_pushnumber(L, tris[i]->GetPoint(1)->x);
+    lua_rawseti(L, -2, 3);
+    lua_pushnumber(L, tris[i]->GetPoint(1)->y);
+    lua_rawseti(L, -2, 4);
+    lua_pushnumber(L, tris[i]->GetPoint(2)->x);
+    lua_rawseti(L, -2, 5);
+    lua_pushnumber(L, tris[i]->GetPoint(2)->y);
+    lua_rawseti(L, -2, 6);
+
+    lua_rawseti(L, -2, i + 1);
+  }
+  for (size_t i = 0; i < verts.size(); ++i)
+    delete verts[i];
+  delete cdt;
   return 1;
 }
 
